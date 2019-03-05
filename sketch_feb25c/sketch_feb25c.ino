ba773826247b03e530568739882c3c12d0495e8f
[dinithi] <[dinithimweerasinghe95@gmail.com>
2019-03-05 19:24:39 +0530

    arduino2
    arduino2

---
+++ sketch_feb25c/sketch_feb25c.ino
@@ -0,0 +1,43 @@
+void setup() {
+  int led1=6;
+  int led2=7;
+  int led3=8;
+  // put your setup code here, to run once:
+
+  pinMode(6,OUTPUT);
+   pinMode(7,OUTPUT);
+    pinMode(8,OUTPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(6,HIGH);
+delay(100);
+digitalWrite(6,LOW);
+delay(100);
+digitalWrite(7,HIGH);
+delay(100);
+digitalWrite(7,LOW);
+delay(100);
+digitalWrite(8,HIGH);
+delay(100);
+digitalWrite(8,LOW);
+delay(100);
+
+
+
+digitalWrite(8,HIGH);
+delay(100);
+digitalWrite(8,LOW);
+delay(100);
+digitalWrite(7,HIGH);
+delay(100);
+digitalWrite(7,LOW);
+delay(100);
+digitalWrite(6,HIGH);
+delay(100);
+digitalWrite(6,LOW);
+delay(100);
+}
